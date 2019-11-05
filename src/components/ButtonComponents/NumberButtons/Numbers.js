028a3c5a0a3e7c985a3a035759e8276c0da3a98a
Jake Regner <jregner20@gmail.com>
2019-11-05 18:30:20 -0500

    Add imported data to state for numbers, operators, and specials

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { numbers } from "../../../data";
 
 //import any components needed
@@ -10,6 +10,7 @@ import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [ numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
